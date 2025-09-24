dd9c6212f6de793e06ff926686f108ea28926ca9
Mustafa Akagündüz <akagunduzmustafa00@gmail.com>
2025-09-24 19:12:43 +0300

    deployment konfigürasyonları yapıldı-4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,12 @@
 
 import CompanyIntroSection from "@/components/home/CompanyIntroSection";
 import PropertiesSection from "@/components/home/PropertiesSection";
-
+//deneme
 export default function Home() {
     return (
         <main className="min-h-screen">
             <CompanyIntroSection />
+
             <PropertiesSection />
         </main>
     );
